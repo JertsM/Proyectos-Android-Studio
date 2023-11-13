9294c3d28ac64df1e2c353d7e929857c7909ef8c
Mauricio Míguez Núñez <mauricioeme46@outlook.com>
2023-11-13 20:44:44 +0100

    Calculadora terminada

--- CalculadoraM/app/src/main/java/com/example/firstapp/MainActivity.kt
+++ CalculadoraM/app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.firstapp.databinding.ActivityMainBinding
@@ -15,34 +16,44 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
         val Texto1 = findViewById<EditText>(R.id.editText)
         val Texto2 = findViewById<EditText>(R.id.editText2)
         val RadioButton = findViewById<RadioButton>(R.id.radioButtonSum)
         val RadioButton2 = findViewById<RadioButton>(R.id.radioButtonSubstract)
 
-        RadioButton.setOnCheckedChangeListener { buttonView, isChecked ->
-            if (isChecked) {
-                val num1 = Texto1.text.toString().toInt()
-                val num2 = Texto2.text.toString().toInt()
+        val radioGroup = findViewById<RadioGroup>(R.id.radioGroup)
 
-                val suma = num1 + num2
+        binding.radioGroup.setOnCheckedChangeListener { group, checkedId ->
+            when (checkedId) {
+                R.id.radioButtonSum -> {
+                    try{
+                        val num1 = Texto1.text.toString().toInt()
+                        val num2 = Texto2.text.toString().toInt()
 
-                val resultado = findViewById<TextView>(R.id.editText3)
-                resultado.text = "Resultado: $suma"
-            }
-        }
+                        val suma = num1 + num2
 
-        RadioButton2.setOnCheckedChangeListener { buttonView, isChecked ->
-            if (isChecked) {
-                val num1 = Texto1.text.toString().toInt()
-                val num2 = Texto2.text.toString().toInt()
+                        val resultado = findViewById<TextView>(R.id.editText3)
+                        resultado.text = suma.toString()
+                    } catch (e: NumberFormatException){
+                     e.stackTrace
+                    }
+                }
 
-                val resta = num1 - num2
+                R.id.radioButtonSubstract -> {
+                    try{
+                        val num1 = Texto1.text.toString().toInt()
+                        val num2 = Texto2.text.toString().toInt()
 
-                val resultado = findViewById<TextView>(R.id.editText3)
-                resultado.text = "Resultado: $resta"
+                        val resta = num1 - num2
+
+                        val resultado = findViewById<TextView>(R.id.editText3)
+                        resultado.text = resta.toString()
+                    } catch (e: NumberFormatException){
+                        e.stackTrace
+                    }
+                }
             }
         }
     }
