416210b251a7922583c5d5e665ba7009d7157a33
Mauricio Míguez Núñez <mauricioeme46@outlook.com>
2024-02-05 20:40:23 +0100

    Update CalculadoraCompose

--- Jetpack Compose/CalculadoraCompose/app/src/main/java/com/example/calculadoracompose/MainActivity.kt
+++ Jetpack Compose/CalculadoraCompose/app/src/main/java/com/example/calculadoracompose/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.calculadoracompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.calculadoracompose.ui.theme.CalculadoraComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,11 +43,42 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         modifier = modifier
     )
 }
-
+/*
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     CalculadoraComposeTheme {
         Greeting("Android")
     }
+
 }
+ */
+@Preview
+@Composable
+fun celdas(modifier: Modifier = Modifier){
+    Column (horizontalAlignment = Alignment.CenterHorizontally){
+        modifier = Modifier.fillMaxSize().padding(16.dp)
+
+
+    }
+    CeldasValores()
+}
+
+@Composable
+fun CeldasValores(){
+    TextField(
+        value = "",
+        onValueChange = {},
+        label = { Text("Número 1") },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+    )
+
+    TextField(
+        value = "",
+        onValueChange = {},
+        label = { Text("Número 2") },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+    )
+}
+
+
